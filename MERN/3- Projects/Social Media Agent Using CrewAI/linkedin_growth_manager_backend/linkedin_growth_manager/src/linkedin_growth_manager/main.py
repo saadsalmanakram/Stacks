2776c3c602eea70882bc899e38d2ba8d86d5cd7b
Saad Salman <saadsalmanakram1@gmail.com>
2024-10-28 12:39:58 +0500

    update

---
+++ MERN/3- Projects/Social Media Agent Using CrewAI/linkedin_growth_manager_backend/linkedin_growth_manager/src/linkedin_growth_manager/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import sys
+from linkedin_growth_manager.crew import LinkedinGrowthManagerCrew
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs'
+    }
+    LinkedinGrowthManagerCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        LinkedinGrowthManagerCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        LinkedinGrowthManagerCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        LinkedinGrowthManagerCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
