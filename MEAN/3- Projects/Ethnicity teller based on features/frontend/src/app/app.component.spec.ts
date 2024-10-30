55a8d36ed658507b6969465e46b9c8bc029a470a
Saad Salman <saadsalmanakram1@gmail.com>
2024-10-30 12:32:49 +0500

    update

--- MEAN/3- Projects/Ethnicity teller based on features/frontend/src/app/app.component.spec.ts
+++ MEAN/3- Projects/Ethnicity teller based on features/frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ethnicity' title`, () => {
+  it(`should have the 'frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ethnicity');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ethnicity');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
 });
