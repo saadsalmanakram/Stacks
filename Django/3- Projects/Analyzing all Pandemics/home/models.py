836a192c181b739a16f5973238d5f60c89e0ee8f
Saad Salman <154062428+saadsalmanakram@users.noreply.github.com>
2024-11-10 16:52:20 +0000

    update

--- Django/3- Projects/Analyzing all Pandemics/home/models.py
+++ Django/3- Projects/Analyzing all Pandemics/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True, blank=True)
+    image = models.ImageField()
+    file = models.FileField()
+
+
+class Product(models.Model):
+    pass
