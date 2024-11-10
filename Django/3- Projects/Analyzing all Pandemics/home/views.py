9e2761445a6a69f3808d2a6147bd10e591270438
Saad Salman <154062428+saadsalmanakram@users.noreply.github.com>
2024-11-10 13:45:59 +0000

    update

--- Django/3- Projects/Analyzing all Pandemics/home/views.py
+++ Django/3- Projects/Analyzing all Pandemics/home/views.py
@@ -4,4 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html")
+    peoples = [
+        {'name' : 'Saad', 'age' : 26},
+        {'name' : 'Salman', 'age' : 22},
+        {'name' : 'Monti', 'age' : 20},
+        {'name' : 'Haider', 'age' : 19},
+        {'name' : 'Javeria', 'age' : 18}
+    ]
+
+    text = """
+         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    """
+    
+    return render(request, "home/index.html", context= {'peoples': peoples, 'text': text})
+
