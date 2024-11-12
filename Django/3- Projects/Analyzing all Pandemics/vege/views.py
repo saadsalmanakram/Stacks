f8abf70410c901f5f38e6c2c0c6260e390f0e7ea
Saad Salman <154062428+saadsalmanakram@users.noreply.github.com>
2024-11-12 10:34:54 +0000

    update

--- Django/3- Projects/Analyzing all Pandemics/vege/views.py
+++ Django/3- Projects/Analyzing all Pandemics/vege/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def receipes(request):
+    if request.method == "POST":
+
+        data = request.POST
+        receipe_image = request.FILES['receipe_image']
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+
+        print(receipe_description)
+        print(receipe_name)
+
     return render(request, 'receipes.html')
 
