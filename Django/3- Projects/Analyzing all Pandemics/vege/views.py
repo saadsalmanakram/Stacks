03ecc1734256b4f6869639fbb3d336e0ccce15a4
Saad Salman <154062428+saadsalmanakram@users.noreply.github.com>
2024-11-14 20:14:49 +0000

    update

--- Django/3- Projects/Analyzing all Pandemics/vege/views.py
+++ Django/3- Projects/Analyzing all Pandemics/vege/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def receipes(request):
@@ -54,6 +55,26 @@ def update_receipe(request, id):
     return render(request, 'update_receipes.html', context)
 
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect('/login/')
+        
+        user = authenticate(username = username, password = password)
+
+        if user is None:
+            messages.error(request, 'Invalid Password')
+            return redirect('/login/')
+        
+        else:
+            login(request, user)
+            return redirect('/receipes/')
+
+
+
     return render(request, 'login.html')
 
 def register(request):
