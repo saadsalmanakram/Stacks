6862d4c54798f79c0959c6dadfbafa546219d0c8
Saad Salman <154062428+saadsalmanakram@users.noreply.github.com>
2024-11-14 20:32:41 +0000

    update

--- Django/3- Projects/Analyzing all Pandemics/vege/views.py
+++ Django/3- Projects/Analyzing all Pandemics/vege/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url="/login/")
 def receipes(request):
     if request.method == "POST":
         data = request.POST
