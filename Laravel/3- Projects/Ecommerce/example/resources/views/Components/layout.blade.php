8a9d16537744541bc4b76aac1f3e357342c532ce
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-18 23:26:28 +0500

    update

---
+++ Laravel/3- Projects/Ecommerce/example/resources/views/Components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Page</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">about</a>
+        <a href="/contact">contact</a>
+    </nav>
+    
+    <?php echo $slot ?>
+</body>
+</html>
---
+++ Laravel/3- Projects/Ecommerce/example/resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello from the Contacts Page</h1>
+</x-layout>
