62dea5241dde3efa6540893c0c2302976b1eb035
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-19 01:36:50 +0500

    Update layout.blade.php

--- Laravel/3- Projects/Ecommerce/example/resources/views/Components/layout.blade.php
+++ Laravel/3- Projects/Ecommerce/example/resources/views/Components/layout.blade.php
@@ -32,7 +32,7 @@
             <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+                <a href="/" class="{{ request() -> is('/') ? "bg-gray-900 text-white":"text-gray-300 hover:bg-gray-700 hover:text-white"}}rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Home</a>
                 <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                 <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
             </div>
