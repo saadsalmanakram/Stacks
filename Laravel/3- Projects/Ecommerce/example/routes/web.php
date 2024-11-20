1d7de9456e8028b8b2202c1e5ce1e15adba124c3
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-20 12:32:50 +0500

    update

--- Laravel/3- Projects/Ecommerce/example/routes/web.php
+++ Laravel/3- Projects/Ecommerce/example/routes/web.php
@@ -2,29 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-class Job{
-    public static function all():array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
-}
+use App\Models\Job;
 
 
 Route::get('/', function () {
