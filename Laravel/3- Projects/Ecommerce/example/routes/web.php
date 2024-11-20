917e788bd9ca76eabb3704890d06636456564358
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-20 11:16:16 +0500

    Update web.php

--- Laravel/3- Projects/Ecommerce/example/routes/web.php
+++ Laravel/3- Projects/Ecommerce/example/routes/web.php
@@ -3,36 +3,42 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'Director',
-        'salary' => '$50,000'
-    ],
-    [
-        'id' => 2,
-        'title' => 'Programmer',
-        'salary' => '$10,000'
-    ],
-    [
-        'id' => 3,
-        'title' => 'Teacher',
-        'salary' => '$40,000'
-    ]
-];
+class Job{
+    public static function all():array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+        ];
+    }
+}
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs)  {
+Route::get('/jobs', function ()  {
     return view('jobs', [
-        'jobs' => $jobs
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
     return view('job',['job' => $job]);
 });
 
