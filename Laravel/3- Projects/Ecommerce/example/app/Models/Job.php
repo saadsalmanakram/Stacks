949ec7eff92e8d4ffe7e49d6cddd11e367d7faa8
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-20 12:39:29 +0500

    update

---
+++ Laravel/3- Projects/Ecommerce/example/app/Models/Job.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+class Job{
+    public static function all():array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+        ];
+    }
+}
