a6ede47e5c7ed878120557d4cd7b130f375156d5
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-21 20:58:26 +0500

    update

--- Laravel/3- Projects/Ecommerce/example/app/Models/Job.php
+++ Laravel/3- Projects/Ecommerce/example/app/Models/Job.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model {
+class Job extends Model{
     protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
 }
