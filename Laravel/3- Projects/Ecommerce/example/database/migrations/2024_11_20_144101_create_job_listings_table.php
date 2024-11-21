dc63443f5f9e6f24dbdb61695e4c4591e56dd81c
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-21 13:09:01 +0500

    Update 2024_11_20_144101_create_job_listings_table.php

--- Laravel/3- Projects/Ecommerce/example/database/migrations/2024_11_20_144101_create_job_listings_table.php
+++ Laravel/3- Projects/Ecommerce/example/database/migrations/2024_11_20_144101_create_job_listings_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('salary');
             $table->timestamps();
         });
     }
