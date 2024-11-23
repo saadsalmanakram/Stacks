111d83f13547bbc86b31a0fb9ccd82799ae9b619
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-23 14:36:19 +0500

    update

--- Laravel/3- Projects/Ecommerce/example/database/factories/JobFactory.php
+++ Laravel/3- Projects/Ecommerce/example/database/factories/JobFactory.php
@@ -17,7 +17,8 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->jobTitle(),
+            'salary' => '$50,000 USD'
         ];
     }
 }
