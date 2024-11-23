a7a95a46d4b3e53e0a53ea0d6d5c7bd6d3553047
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-23 17:42:21 +0500

    update

--- Laravel/3- Projects/Ecommerce/example/database/factories/EmployerFactory.php
+++ Laravel/3- Projects/Ecommerce/example/database/factories/EmployerFactory.php
@@ -17,7 +17,7 @@ class EmployerFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->company
         ];
     }
 }
--- Laravel/3- Projects/Ecommerce/example/database/factories/JobFactory.php
+++ Laravel/3- Projects/Ecommerce/example/database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
