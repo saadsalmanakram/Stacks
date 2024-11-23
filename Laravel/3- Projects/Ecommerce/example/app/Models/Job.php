53e28d46a46c192c8a8f8219571b6f8416d783ff
Saad Salman <saadsalmanakram1@gmail.com>
2024-11-23 14:34:13 +0500

    Update Job.php

--- Laravel/3- Projects/Ecommerce/example/app/Models/Job.php
+++ Laravel/3- Projects/Ecommerce/example/app/Models/Job.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model{
+    use HasFactory;
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
