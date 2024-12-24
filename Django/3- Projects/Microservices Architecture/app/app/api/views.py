4e38eb632fe177ad88fd2043639bff296e89eb38
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-24 13:07:37 +0500

    update

--- Django/3- Projects/Microservices Architecture/app/app/api/views.py
+++ Django/3- Projects/Microservices Architecture/app/app/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
-# Create your views here.
+class RoomView(generics.CreateAPIView):
+	queryset = Room.objects.all()
+	serializer_class = RoomSerializer
 
-def main(request):
-	return HttpResponse("<h2>Print Hello World</h2>")
+
+
