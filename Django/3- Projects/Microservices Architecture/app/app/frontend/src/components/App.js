9396ce610111069309961b97a5fde8ef01aad369
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-25 01:12:02 +0500

    Update App.js

--- Django/3- Projects/Microservices Architecture/app/app/frontend/src/components/App.js
+++ Django/3- Projects/Microservices Architecture/app/app/frontend/src/components/App.js
@@ -1,1 +1,12 @@
-App
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <h1> Testing React Codes </h1>;
+	}
+}
