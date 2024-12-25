ca80346cdc17816a062d59adf7a03441761d2172
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-26 01:16:57 +0500

    update

---
+++ Django/3- Projects/Microservices Architecture/app/app/frontend/static/frontend/main.js
@@ -0,0 +1,2 @@
+/*! For license information please see main.js.LICENSE.txt */
+(()=>{var __webpack_modules__={"./src/index.js":()=>{eval("throw new Error(\"Module parse failed: 'import' and 'export' may appear only with 'sourceType: module' (1:0)\\nFile was processed with these loaders:\\n * ./node_modules/babel-loader/lib/index.js\\nYou may need an additional loader to handle the result of these loaders.\\n> import App from \\\"./components/App\\\";\");\n\n//# sourceURL=webpack://frontend/./src/index.js?")}},__webpack_exports__={};__webpack_modules__["./src/index.js"]()})();
