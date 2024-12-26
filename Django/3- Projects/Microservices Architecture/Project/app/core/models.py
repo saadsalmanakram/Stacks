84dd53daa2fe9d5ee837af93ba5604f1e8fe115f
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-26 09:12:28 +0500

    Update models.py

--- Django/3- Projects/Microservices Architecture/Project/app/core/models.py
+++ Django/3- Projects/Microservices Architecture/Project/app/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Custom(models.Model):
+	city = models.CharField(max_length=100, blank=True, null=True)
+	state = models.CharField(max_length=100, blank=True, null=True)
+	address = models.TextField(blank=True, null=True)
+	phone = models.CharField(max_length=15, blank=True, null=True)
+
+	def __str__(self):
+		return self.username
