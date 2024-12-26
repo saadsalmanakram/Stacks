091a37ae921ce8f02ffe4f1514e84c8ba8ffdd2f
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-26 09:40:37 +0500

    update

--- Django/3- Projects/Microservices Architecture/Project/app/core/models.py
+++ Django/3- Projects/Microservices Architecture/Project/app/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Custom(models.Model):
+class Custom(AbstractUser):
 	city = models.CharField(max_length=100, blank=True, null=True)
 	state = models.CharField(max_length=100, blank=True, null=True)
 	address = models.TextField(blank=True, null=True)
