20c10f64cc86b504ac58c697c9e627ec70851e73
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-31 01:15:23 +0500

    Update admin.py

--- Django/3- Projects/Microservices Architecture/Project/app/core/admin.py
+++ Django/3- Projects/Microservices Architecture/Project/app/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import UserAdmin
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'state', 'address', 'phone', 'is_staff', 'is_active')}
+		),
+	)
