6f81aca28930deece1a918ce3ee518aeb7547d13
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-03 00:18:42 +0500

    Update models.py

--- Django/3- Projects/Monolithic Architecture/PROJECT/app/core/models.py
+++ Django/3- Projects/Monolithic Architecture/PROJECT/app/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+	city = models.CharField(max_length=100, blank=True, null=True)
+	state = models.CharField(max_length=100, blank=True, null=True)
+	address = models.TextField(blank=True, null=True)
+	phone = models.CharField(max_length=15, blank=True, null=True)
