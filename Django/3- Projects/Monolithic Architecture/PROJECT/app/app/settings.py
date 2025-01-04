267ff2cd03427206473ec4c801c795599be498e7
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-04 14:24:31 +0500

    update

--- Django/3- Projects/Monolithic Architecture/PROJECT/app/app/settings.py
+++ Django/3- Projects/Monolithic Architecture/PROJECT/app/app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'shop_app'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.CustomUser'
