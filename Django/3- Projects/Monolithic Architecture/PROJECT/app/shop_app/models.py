b40bbd700458749d50ec827f1ff567d47fdab50f
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-04 14:54:20 +0500

    Update models.py

--- Django/3- Projects/Monolithic Architecture/PROJECT/app/shop_app/models.py
+++ Django/3- Projects/Monolithic Architecture/PROJECT/app/shop_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+	CATEGORY = (("Electronics", "ELECTRONS"),
+		        ("Groceries", "GROCERIES"),
+		        ("Clothings", "CLOTHINGS")
+		        )
+
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(blank=True, null=True)
+	image = models.ImageField(upload_to="img")
+	description = models.TextField(blank=True, null=True)
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	category = models.CharField(max_length=15, choices=CATEGORY, blank=True, null=True)
+
+
+	def __str__(self):
+		return self.name
+
+
+	def save(self, *args, **kwargs):
+
+		if not self.slug:
+			self.slug = slugify(self.name)
+			unique_slug = self.slug
+			counter = 1
+			if Product.object.filter(slug=unique_slug).exists():
+				unique_slug = f'{self.slug}-{counter}'
+				counter += 1
+			self.slug = unique_slug
+
+		super().save(*args, **kwargs)
+
