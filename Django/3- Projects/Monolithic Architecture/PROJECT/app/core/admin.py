92991d13d3a32f3a61e89d0f14bc64658717bfc5
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-04 13:35:22 +0500

    Update admin.py

--- Django/3- Projects/Monolithic Architecture/PROJECT/app/core/admin.py
+++ Django/3- Projects/Monolithic Architecture/PROJECT/app/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide'),
+			'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'state', 'address', 'phone', 'is_staff', 'is_active')}
+		)
+	)
+
+	
