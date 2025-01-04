60a18970c451339fa795e569ce4775b5cc5ed337
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-04 13:27:02 +0500

    Update models.py

--- Django/3- Projects/Monolithic Architecture/PROJECT/app/core/models.py
+++ Django/3- Projects/Monolithic Architecture/PROJECT/app/core/models.py
@@ -7,3 +7,7 @@ class CustomUser(AbstractUser):
 	state = models.CharField(max_length=100, blank=True, null=True)
 	address = models.TextField(blank=True, null=True)
 	phone = models.CharField(max_length=15, blank=True, null=True)
+
+	def __str__(self):
+		return self.username
+		
