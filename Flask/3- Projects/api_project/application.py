04d5fa5352b501e1b11274f090dba5a442a1547c
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-05 18:46:50 +0500

    update

--- Flask/3- Projects/api_project/application.py
+++ Flask/3- Projects/api_project/application.py
@@ -1,5 +1,18 @@
 from flask import Flask 
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Drink(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80), unique=True, nullable=False)
+	description = db.Column(db.String(120))
+
+	def __repr__(self):
+		return f"{self.name} - {self.description}"
 
 @app.route('/')
 def index():
@@ -7,6 +20,5 @@ def index():
 
 @app.route('/drinks')
 def get_drinks():
-	return drinks
+	return {"drinks": "drink data"}
 
-	
