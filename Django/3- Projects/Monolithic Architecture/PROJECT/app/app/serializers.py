586aefa9f897daa135a3e8d2cb49bc2375fe5abe
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-06 00:47:33 +0500

    update

--- Django/3- Projects/Monolithic Architecture/PROJECT/app/app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serilaizers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Product
-		fields = ["id", "name", "slug", "image", "description", "category", "price"]
-
-		
--- Django/3- Projects/Monolithic Architecture/PROJECT/app/shop_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlspatterns = [
-     path("products", views.products, name="products")
-]
-
