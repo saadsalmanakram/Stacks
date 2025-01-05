aed913d70231ae36b212401c51250c23dcbd3461
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-05 20:18:27 +0500

    update

--- Flask/3- Projects/api_project/application.py
+++ Flask/3- Projects/api_project/application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 # Initialize Flask app
@@ -32,6 +32,28 @@ def get_drinks():
         output.append(drink_data)
 
     return {"drinks": output}
+
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return {"name": drink.name, "description": drink.description}
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "not found"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"message": "yeet!@"}
 
 # Run the app
 if __name__ == '__main__':
