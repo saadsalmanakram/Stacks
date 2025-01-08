32c40c064a7ff771efebbc3f700c7fd032bcb245
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-09 02:02:10 +0500

    Update index.js

--- MERN/3- Projects/Project/server/index.js
+++ MERN/3- Projects/Project/server/index.js
@@ -3,5 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express');
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+
 
