731dd5eaf2259d60d4e624ff2b5929e2979e1ad3
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-10 16:36:58 +0500

    Update posts.js

--- MERN/3- Projects/Project/server/controllers/posts.js
+++ MERN/3- Projects/Project/server/controllers/posts.js
@@ -1,7 +1,16 @@
+import PostMessage from '../models/postMessage.js';
 
 
-export const getPosts = (req, res) => {
-	res.send('This works!');
+export const getPosts = async (req, res) => {
+	try {
+		const postMessages = await PostMessage.find();
+
+		console.log(postMessages);
+
+		res.status(200).json(postMessages);
+	} catch (error) {
+
+	}
 }
 
 export const createPost = (req, res) => {
