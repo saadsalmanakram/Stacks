cc1b88122e6b4d6bacb9ee40ac6c3c648a9bfc29
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-10 17:38:53 +0500

    Update App.js

--- MERN/3- Projects/Project/frontend/src/App.js
+++ MERN/3- Projects/Project/frontend/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     <Container maxidth="lg">
         <AppBar position="static" color="inherit">
             <Typography variant="h2" align="center">Memories</Typography>
-
+            <img src={memories} alt="memories" height="60" />
         </AppBar>
     </Container>
   )
