fe69725a7ac314f48bfcdcca2fd75e4308742907
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-10 16:57:58 +0500

    Update posts.js

--- MERN/3- Projects/Project/server/controllers/posts.js
+++ MERN/3- Projects/Project/server/controllers/posts.js
@@ -13,13 +13,15 @@ export const getPosts = async (req, res) => {
 	}
 }
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
 	const post = req.body;
 
 	const newPost = new PostMessage(post);
 
 	try {
+        await newPost.save();
 
+        res.status(201).json(newPost)
 	} catch (error) {
 
 	}
