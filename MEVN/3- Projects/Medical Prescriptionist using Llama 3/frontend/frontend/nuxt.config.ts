0ca7e28c1c23e8f0e8bde98fd354247e3aca3c55
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-29 13:30:12 +0500

    update

--- MEVN/3- Projects/Medical Prescriptionist using Llama 3/frontend/frontend/nuxt.config.ts
+++ MEVN/3- Projects/Medical Prescriptionist using Llama 3/frontend/frontend/nuxt.config.ts
@@ -1,5 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
