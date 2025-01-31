15b37bb56e82952614c1868f76402566a0480ad3
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-31 15:43:39 +0500

    update

--- Laravel/3- Projects/Ecommerce/example/resources/views/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Contact Page
-    </x-slot:heading>
-    <h1>Hello from the Contact Page</h1>
-</x-layout>
--- Laravel/3- Projects/Ecommerce/example/resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Home Page
-    </x-slot:heading>
-
-</x-layout>
