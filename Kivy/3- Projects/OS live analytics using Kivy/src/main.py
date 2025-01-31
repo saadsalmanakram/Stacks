c5c26f3cc07da43260ce059fa680b3807f344508
Saad Salman <saadsalmanakram1@gmail.com>
2025-02-01 01:09:10 +0500

    update

---
+++ Kivy/3- Projects/OS live analytics using Kivy/src/main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Hello, Kivy!')
+
+if __name__ == '__main__':
+    MyApp().run()
