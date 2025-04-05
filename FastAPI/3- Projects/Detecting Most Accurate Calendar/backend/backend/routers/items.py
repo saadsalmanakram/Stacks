158a89ab8ce47e1c0b6f557401167b57008a806c
Saad Salman <saadsalmanakram1@gmail.com>
2025-04-05 13:36:51 +0500

    update

--- FastAPI/3- Projects/Detecting Most Accurate Calendar/backend/backend/routers/items.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from schemas.item import Item
-
-router = APIRouter()
-
-@router.post("/items/")
-def create_item(item: Item):
-    return {"item_name": item.name}
--- MEVN/3- Projects/Medical Prescriptionist using Llama 3/frontend/frontend/src/main.ts
+++
@@ -1,7 +0,0 @@
-// src/main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-
-const app = createApp(App)
-app.mount('#app')
